73d63c7d3065322e22f33f6209b1778c584d26a9
gioboa <giorgiob.boa@gmail.com>
2024-03-30 22:27:04 +0100

    feat: add HorizontalMirror icon

---
+++ src/components/icons/HorizontalMirror.tsx
@@ -0,0 +1,15 @@
+export const HorizontalMirror = () => (
+  <svg
+    fill="#ccc"
+    viewBox="0 0 20 20"
+    class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+  >
+    <g fill-rule="evenodd">
+      <path d="M5 0h10l-5 7zM0 9h20v2H0z"></path>
+      <path
+        fill-rule="nonzero"
+        d="M10 13l5 7H5l5-7zm-3.373 6.163h6.746L10 14.44l-3.373 4.722z"
+      ></path>
+    </g>
+  </svg>
+);
