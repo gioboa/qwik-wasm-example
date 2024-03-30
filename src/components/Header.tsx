098b1f34e50553eef8a09f0f4100707cedf94471
gioboa <giorgiob.boa@gmail.com>
2024-03-30 22:53:56 +0100

    chore: remove button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,29 +6,7 @@ export const Header = component$(() => {
     <nav class="fixed top-0 z-50 w-full border-b border-gray-200 dark:border-gray-700">
       <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
-          <div class="flex items-center justify-start rtl:justify-end">
-            <button
-              data-drawer-target="logo-sidebar"
-              data-drawer-toggle="logo-sidebar"
-              aria-controls="logo-sidebar"
-              type="button"
-              class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 sm:hidden"
-            >
-              <span class="sr-only">Open sidebar</span>
-              <svg
-                class="h-6 w-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  clip-rule="evenodd"
-                  fill-rule="evenodd"
-                  d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                ></path>
-              </svg>
-            </button>
+          <div class="flex items-center justify-start">
             <a href="https://flowbite.com" class="ms-2 flex md:me-24">
               <Qwik />
               <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl">
