6a22efaad8cfa180ab519765eb2a2a22d091d10c
gioboa <giorgiob.boa@gmail.com>
2024-04-05 08:28:54 +0200

    refactor: use async await

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,13 +23,12 @@ export default component$(() => {
         />
         <button
           class="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick$={() => {
+          onClick$={async () => {
             // init will reuse a cached version if present
-            init().then(() => {
-              const startTime = performance.now();
-              resultSig.value = fib(fibonacciNumSig.value).toString();
-              timeSig.value = performance.now() - startTime;
-            });
+            await init();
+            const startTime = performance.now();
+            resultSig.value = fib(fibonacciNumSig.value).toString();
+            timeSig.value = performance.now() - startTime;
           }}
         >
           Calculate
